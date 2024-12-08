c7ff5fed39a0d671b220de752feccffe62ce964e
Ekolojikkredi <ekolojikkreditubitak@gmail.com>
2024-12-08 15:05:22 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('kayit-ol').addEventListener('click', function() {
+    alert("Kayıt Ol butonuna tıkladınız.");
+});
+
+document.getElementById('veri-girisi').addEventListener('click', function() {
+    alert("Veri Girişi butonuna tıkladınız.");
+});
+
+document.getElementById('veri-gorme').addEventListener('click', function() {
+    alert("Veri Görme butonuna tıkladınız.");
+});
